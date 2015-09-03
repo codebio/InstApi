fdac017d1f2a73611e3b6ef32bbc416a98c5ec08
abr4xas <me@abr4xas.org>
2015-09-02 23:08:01 -0430

    actualizando repo

--- recursos/sample.php
+++ recursos/sample.php
@@ -1,27 +1,29 @@
 <?php
+
 /*
  * Filename: sample.php
  * Description: Ejemplo para consumo de la API de instapago
  * @author Personal de Instapago
  * @version 1.5.4
  */
+
 $url = 'https://api.instapago.com/payment';
 $fields = array(
-    "KeyID" => "", //requerido
-    "PublicKeyId" => "", //requerido
-    "Amount" => "1", //requerido
-    "Description" => "Un cobro de prueba", //requerido
-    "CardHolder" => "Lorem Ipsum", //requerido
-    "CardHolderId" => "12345678", //requerido
-    "CardNumber" => "8244001100110011", //requerido
-    "CVC" => "123", //requerido
-    "ExpirationDate" => "10/2015", //requerido
-    "StatusId" => "1", //requerido
-    "IP" => "192.168.30.10", //requerido
-    "Address" => "Urb Lipsum",
-    "City" => "Valencia",
-    "ZipCode" => "2001",
-    "State" => "Carabobo"
+    "KeyID"             => "", //requerido
+    "PublicKeyId"       => "", //requerido
+    "Amount"            => "1", //requerido
+    "Description"       => "Un cobro de prueba", //requerido
+    "CardHolder"        => "Lorem Ipsum", //requerido
+    "CardHolderId"      => "12345678", //requerido
+    "CardNumber"        => "8244001100110011", //requerido
+    "CVC"               => "123", //requerido
+    "ExpirationDate"    => "10/2015", //requerido
+    "StatusId"          => "1", //requerido
+    "IP"                => "192.168.30.10", //requerido
+    "Address"           => "Urb Lipsum",
+    "City"              => "Valencia",
+    "ZipCode"           => "2001",
+    "State"             => "Carabobo"
 );
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$url );
@@ -31,5 +33,4 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $server_output = curl_exec ($ch);
 curl_close ($ch);
 echo $server_output;
-
 ?>
