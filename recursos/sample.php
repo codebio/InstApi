2b03c61680bdd22545e7b820de723c159a11bc03
abr4xas <me@abr4xas.org>
2015-09-16 20:10:29 -0430

    Agregado formulario de pruebas, nada fancy

--- recursos/sample.php
+++ recursos/sample.php
@@ -7,23 +7,42 @@
  * @version 1.5.4
  */
 
+$CardHolder     = trim($_POST["cardHolder"]);
+$CardHolderId   = trim($_POST["cardHolderId"]);
+$CardNumber     = trim($_POST["cardNumber"]);
+$CVC            = trim($_POST["cardCVC"]);
+$ExpirationDate = trim($_POST["cardExpiry"]);
+
+
+// https://www.chriswiegman.com/2014/05/getting-correct-ip-address-php/
+function get_ip() {
+    if ( function_exists( 'apache_request_headers' ) ) {
+        $headers = apache_request_headers();
+    } else {
+        $headers = $_SERVER;
+    }
+    if (array_key_exists( 'X-Forwarded-For', $headers ) && filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ) {
+        $the_ip = $headers['X-Forwarded-For'];
+    }elseif (array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) && filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 )) {
+        $the_ip = $headers['HTTP_X_FORWARDED_FOR'];
+    }else {
+        $the_ip = filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 );
+    }
+    return $the_ip;
+}
 $url = 'https://api.instapago.com/payment';
 $fields = array(
-    "KeyID"             => "", //requerido
-    "PublicKeyId"       => "", //requerido
-    "Amount"            => "1", //requerido
-    "Description"       => "Un cobro de prueba", //requerido
-    "CardHolder"        => "Lorem Ipsum", //requerido
-    "CardHolderId"      => "12345678", //requerido
-    "CardNumber"        => "8244001100110011", //requerido
-    "CVC"               => "123", //requerido
-    "ExpirationDate"    => "10/2015", //requerido
-    "StatusId"          => "1", //requerido
-    "IP"                => "192.168.30.10", //requerido
-    "Address"           => "Urb Lipsum",
-    "City"              => "Valencia",
-    "ZipCode"           => "2001",
-    "State"             => "Carabobo"
+    "KeyID"             => "", //required
+    "PublicKeyId"       => "", //required
+    "Amount"            => "200", //required
+    "Description"       => "Un cobro de prueba", //required
+    "CardHolder"        => $CardHolder, //required
+    "CardHolderId"      => $CardHolderId, //required
+    "CardNumber"        => $CardNumber, //required
+    "CVC"               => $CVC, //required
+    "ExpirationDate"    => $ExpirationDate, //required
+    "StatusId"          => "2", //required
+    "IP"                => get_ip() //required
 );
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$url );
@@ -32,5 +51,62 @@ curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($fields));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $server_output = curl_exec ($ch);
 curl_close ($ch);
-echo $server_output;
+$obj = json_decode($server_output);
+$code = $obj->code;
+$msgerror = '';
+switch($code){
+    case 400:
+        $msgerror= 'Error al validar los datos enviados..';
+        break;
+    case 401:
+        $msgerror= 'Error de autenticación, ha ocurrido un error con las llaves utilizadas..';
+        break;
+    case 403:
+        $msgerror= 'Pago Rechazado por el banco..';
+        break;
+    case 500:
+        $msgerror= 'Ha Ocurrido un error interno dentro del servidor..';
+        break;
+    case 503:
+        $msgerror= 'Ha Ocurrido un error al procesar los parámetros de entrada. Revise los datos enviados y vuelva a intentarlo..';
+        break;
+    case 201:
+        $msg_banco  = $obj->message;
+        $voucher  = $obj->voucher;
+        $voucher = html_entity_decode($voucher);
+        $id_pago  = $obj->id;
+        $reference  = $obj->reference;
+        break;
+    default:
+        $msgerror='Error inesperado, Imposible determinar. Contacte al Administrador';
+        break;
+}
+if(!empty($msgerror)){
+    echo'
+        <div class="alert alert-danger" role="alert"><p>'.$msgerror.'</p></div>';
+    die();
+}else{
+    echo'
+    <div class="panel panel-success">
+        <div class="panel-heading">Respuesta Transacción</div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-xs-7 col-md-7">
+                        <h3>Respuesta del Banco</h3>
+                        <p>
+                            <strong>Mensaje del Banco</strong>: '.$msg_banco.'<br/>
+                            <strong>ID del Pago</strong>: '.$id_pago.'<br/>
+                            <strong>ID Referencia</strong>: '.$reference.'<br/>
+                        </p>
+                    </div>
+                    <div class="col-xs-5 col-md-5 pull-right">
+                        <h3>Voucher</h3>
+                        '.$voucher.'
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>';
+}
+
 ?>
